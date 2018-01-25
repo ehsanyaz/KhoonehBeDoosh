d76a2a80e6402077f950d128352ca28db9b1467e
ShahoBashoki <bashoki.shaho@gmail.com>
2018-01-26 00:34:30 +0330

    shaho v5.5

--- app/src/main/java/ir/ac/bonabu/khoonehbedoosh/Map.java
+++ app/src/main/java/ir/ac/bonabu/khoonehbedoosh/Map.java
@@ -1,17 +1,74 @@
 package ir.ac.bonabu.khoonehbedoosh;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class Map extends Activity
+public class Map extends Fragment implements OnMapReadyCallback
 {
+    GoogleMap mGoogleMAp;
+    MapView mMapView;
+    View mView;
+
+    public Map()
+    {
+
+    }
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState)
+    public void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        mView=inflater.inflate(R.layout.activity_map,container,false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view,Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView=(MapView) mView.findViewById(R.id.map);
+
+        if(mMapView!=null)
+        {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMAp=googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689248,-74.044502)).title("Statue of Liberty").snippet("I hope to go there some day"));
+
+        CameraPosition Liberty=CameraPosition.builder().target(new LatLng(40.689247,-74.044502)).zoom(16).bearing(0).tilt(45).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
     }
 }
