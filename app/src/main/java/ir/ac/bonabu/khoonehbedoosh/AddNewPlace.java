1f7b17317fb44f1d946bad1c474e0912ff61fbc3
Ehsan <yazdani.ehsan95@gmail.com>
2018-01-25 23:09:26 +0330

    Ehsan v 12

--- app/src/main/java/ir/ac/bonabu/khoonehbedoosh/AddNewPlace.java
+++ app/src/main/java/ir/ac/bonabu/khoonehbedoosh/AddNewPlace.java
@@ -1,13 +1,18 @@
 package ir.ac.bonabu.khoonehbedoosh;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class AddNewPlace extends AppCompatActivity {
+public class AddNewPlace extends Activity {
+
+    public static double x=0;
+    public static double y=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_place);
+
+
     }
 }
