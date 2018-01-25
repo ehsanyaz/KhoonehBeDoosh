a6f021eeeb326c706060f3cd3673605ae7beb776
Ehsan <yazdani.ehsan95@gmail.com>
2018-01-25 18:59:25 +0330

    Ehsan v 7

---
+++ app/src/main/java/ir/ac/bonabu/khoonehbedoosh/Splash.java
@@ -0,0 +1,17 @@
+package ir.ac.bonabu.khoonehbedoosh;
+
+
+
+        import android.app.Activity;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+    }
+}
