0d1d2c3c9165d059de7105f31857cb37ff9de944
Ehsan <yazdani.ehsan95@gmail.com>
2018-01-25 19:11:23 +0330

    Ehsan v 8

---
+++ app/src/main/java/ir/ac/bonabu/khoonehbedoosh/CutomerActivity.java
@@ -0,0 +1,40 @@
+package ir.ac.bonabu.khoonehbedoosh;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CutomerActivity extends Activity {
+
+    private Button showMap;
+    private Button FilterMAp;
+
+    private Typeface typeface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_activity);
+
+        showMap=(Button) findViewById(R.id.customer_activity_Map_btn);
+        //TODO:complete here
+
+        FilterMAp=(Button)findViewById(R.id.customer_activity_findPlace_btn);
+        FilterMAp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getApplicationContext(),FindPlace.class);
+                startActivity(i);
+
+            }
+        });
+
+        typeface=Typeface.createFromAsset(getAssets(),"Fonts/font.ttf");
+        FilterMAp.setTypeface(typeface);
+        showMap.setTypeface(typeface);
+    }
+}
