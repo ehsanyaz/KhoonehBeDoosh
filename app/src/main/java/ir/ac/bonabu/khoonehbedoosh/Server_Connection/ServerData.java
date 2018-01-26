c97a740ab300fc770e97636b88bde8f3902f1b37
Abolfazl 7x <aboolfazl46@gmail.com>
2018-01-26 17:49:04 +0330

    Abolfazl v2.0

---
+++ app/src/main/java/ir/ac/bonabu/khoonehbedoosh/Server_Connection/ServerData.java
@@ -0,0 +1,242 @@
+package ir.ac.bonabu.khoonehbedoosh.Server_Connection;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import ir.ac.bonabu.khoonehbedoosh.CutomerActivity;
+import ir.ac.bonabu.khoonehbedoosh.OwenerActivity;
+import ir.ac.bonabu.khoonehbedoosh.User;
+
+/**
+ * Created by Abolfazl 7x on 1/25/2018.
+ */
+
+public class ServerData {
+
+    final int SIGN_IN = 0;
+    final int SIGN_UP = 1;
+    boolean check = true;
+    Context context;
+    ProgressDialog progressDialog;
+    int cu = 0;
+    String ServerUploadPath = "http://530d2d83.ngrok.io/Php/kbd/test.php";
+    User.signUp user;
+    User.signIn signIn;
+
+    public ServerData(Context context, User.signUp user, int cu) {
+        this.context = context;
+        this.user = user;
+        this.cu = cu;
+
+    }
+
+    public ServerData(Context context, User.signIn user, int cu) {
+        this.context = context;
+        this.signIn = user;
+        this.cu = cu;
+    }
+
+
+    public void upload() {
+
+        class AsyncTaskUploadClass extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+
+                super.onPreExecute();
+
+               progressDialog = ProgressDialog.show(context, "Connecting...", "Please Wait", false, false);
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+               progressDialog.dismiss();
+                Log.w("Server", result);
+                if (cu == SIGN_UP || cu == SIGN_IN) {
+                    if (result.contains("successful") && result.contains("user")) {
+                        context.startActivity(new Intent(context, CutomerActivity.class));
+                    } else if (result.contains("successful") && result.contains("costumer")) {
+                        context.startActivity(new Intent(context, OwenerActivity.class));
+                    } else {
+                        AlertDialog.Builder msg = new AlertDialog.Builder(context)
+                                .setIcon(android.R.drawable.ic_delete)
+                                .setNegativeButton("باشه", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                    }
+                                });
+                        if (cu == SIGN_UP) {
+                            msg.setTitle("ثبت نام ناموفق")
+                                    .setMessage("مشکل ارتباط با سرور");
+                        } else {
+                            msg.setTitle("ورود ناموفق")
+                                    .setMessage("نام کاربری یا گذر واژه نادرست وارد شده اند");
+                        }
+                        msg.create().show();
+                    }
+                } else {
+
+                }
+                // Log.w("Server", result);
+
+
+            }
+
+
+            @Override
+            protected String doInBackground(Void... params) {
+
+                SendProcessClass imageProcessClass = new SendProcessClass();
+                String FinalData = "";
+                if (cu == SIGN_UP) {
+                    HashMap<String, String> HashMapParams = signUpPreoare();
+                    FinalData = imageProcessClass.ImageHttpRequest(ServerUploadPath, HashMapParams);
+                } else if (cu == SIGN_IN) {
+                    HashMap<String, String> HashMapParams = signInPreoare();
+                    FinalData = imageProcessClass.ImageHttpRequest(ServerUploadPath, HashMapParams);
+                }
+
+                return FinalData;
+            }
+        }
+        AsyncTaskUploadClass AsyncTaskUploadClassOBJ = new AsyncTaskUploadClass();
+
+        AsyncTaskUploadClassOBJ.execute();
+    }
+
+    public class SendProcessClass {
+
+        public String ImageHttpRequest(String requestURL, HashMap<String, String> PData) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            try {
+
+                URL url;
+                HttpURLConnection httpURLConnectionObject;
+                OutputStream OutPutStream;
+                BufferedWriter bufferedWriterObject;
+                BufferedReader bufferedReaderObject;
+                int RC;
+
+                url = new URL(requestURL);
+
+                httpURLConnectionObject = (HttpURLConnection) url.openConnection();
+
+                httpURLConnectionObject.setReadTimeout(30000);
+
+                httpURLConnectionObject.setConnectTimeout(30000);
+
+                httpURLConnectionObject.setRequestMethod("POST");
+
+                httpURLConnectionObject.setDoInput(true);
+
+                httpURLConnectionObject.setDoOutput(true);
+
+                OutPutStream = httpURLConnectionObject.getOutputStream();
+
+                bufferedWriterObject = new BufferedWriter(
+
+                        new OutputStreamWriter(OutPutStream, "UTF-8"));
+
+                bufferedWriterObject.write(bufferedWriterDataFN(PData));
+
+                bufferedWriterObject.flush();
+
+                bufferedWriterObject.close();
+
+                OutPutStream.close();
+
+                RC = httpURLConnectionObject.getResponseCode();
+
+                if (RC == HttpsURLConnection.HTTP_OK) {
+
+                    bufferedReaderObject = new BufferedReader(new InputStreamReader(httpURLConnectionObject.getInputStream()));
+
+                    stringBuilder = new StringBuilder();
+
+                    String RC2;
+
+                    while ((RC2 = bufferedReaderObject.readLine()) != null) {
+
+                        stringBuilder.append(RC2);
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return stringBuilder.toString();
+        }
+
+        private String bufferedWriterDataFN(HashMap<String, String> HashMapParams) throws UnsupportedEncodingException {
+
+            StringBuilder stringBuilderObject;
+
+            stringBuilderObject = new StringBuilder();
+
+            for (Map.Entry<String, String> KEY : HashMapParams.entrySet()) {
+
+                if (check)
+
+                    check = false;
+                else
+                    stringBuilderObject.append("&");
+
+                stringBuilderObject.append(URLEncoder.encode(KEY.getKey(), "UTF-8"));
+
+                stringBuilderObject.append("=");
+
+                stringBuilderObject.append(URLEncoder.encode(KEY.getValue(), "UTF-8"));
+            }
+
+            return stringBuilderObject.toString();
+        }
+
+    }
+
+    public HashMap<String, String> signUpPreoare() {
+        HashMap<String, String> HashMapParams = new HashMap<String, String>();
+        HashMapParams.put("u[name]", user.getName());
+        HashMapParams.put("u[lastname]", user.getLastname());
+        HashMapParams.put("u[email]", user.getEmail());
+        HashMapParams.put("u[username]", user.getUsername());
+        HashMapParams.put("u[password]", user.getPassword());
+        HashMapParams.put("u[phone]", user.getPhone());
+       // HashMapParams.put("u[nacode]", user.getNacode());
+        HashMapParams.put("action", "1");
+        return HashMapParams;
+    }
+
+    public HashMap<String, String> signInPreoare() {
+        HashMap<String, String> HashMapParams = new HashMap<String, String>();
+        HashMapParams.put("u[username]", signIn.getUsername());
+        HashMapParams.put("u[password]", signIn.getPassword());
+        Log.e("server", signIn.getUsername());
+        Log.e("server", signIn.getPassword());
+        HashMapParams.put("action", "0");
+        return HashMapParams;
+    }
+}
--- app/src/main/java/ir/ac/bonabu/khoonehbedoosh/Splash.java
+++ app/src/main/java/ir/ac/bonabu/khoonehbedoosh/Splash.java
@@ -5,38 +5,80 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
-import ir.ac.bonabu.khoonehbedoosh.Server_Connection.SendUser;
+import ir.ac.bonabu.khoonehbedoosh.Server_Connection.ServerData;
 
 public class Splash extends Activity {
-    Button signin;
-    LinearLayout signup_Layot;
+    final int SIGN_IN=0;
+    final int SIGN_UP=1;
+    Button signin_laybtn;
+    EditText signup_u, signup_p, signup_e, signup_n, signup_ln, signup_ph, signup_nc,signin_p,signin_u;
+    LinearLayout signup_Layot,signin_Layot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        signin = (Button) findViewById(R.id.open_layout);
-        signup_Layot = (LinearLayout) findViewById(R.id.sigin_lay);
-        signin.setOnClickListener(new View.OnClickListener() {
+        signin_laybtn = (Button) findViewById(R.id.open_layout);
+        signup_Layot = (LinearLayout) findViewById(R.id.sigup_lay);
+        signin_Layot = (LinearLayout) findViewById(R.id.sigin_lay);
+        signup_u=(EditText)findViewById(R.id.signup_u);
+        signup_p=(EditText)findViewById(R.id.signup_p);
+        signup_n=(EditText)findViewById(R.id.signup_n);
+        signup_ln=(EditText)findViewById(R.id.signup_ln);
+        signup_e=(EditText)findViewById(R.id.signup_e);
+        signup_ph=(EditText)findViewById(R.id.signup_ph);
+        signup_nc=(EditText)findViewById(R.id.signup_nc);
+        signin_u=(EditText)findViewById(R.id.signin_u);
+        signin_p=(EditText)findViewById(R.id.signin_p);
+        signin_laybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signin.setVisibility(View.GONE);
+                signin_laybtn.setVisibility(View.GONE);
+                signin_Layot.setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.go_toSignup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signin_Layot.setVisibility(View.GONE);
                 signup_Layot.setVisibility(View.VISIBLE);
             }
         });
+        findViewById(R.id.go_toSignin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signup_Layot.setVisibility(View.GONE);
+                signin_Layot.setVisibility(View.VISIBLE);
 
-        User user = new User("a", "b", "c", "d", "e", "0914", "0914");
-        SendUser sendUser = new SendUser(Splash.this, user);
-        //   sendUser.ImageUploadToServerFunction();
+            }
+        });
+        findViewById(R.id.sigup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User.signUp user = new User.signUp(signup_n.getText().toString(), signup_ln.getText().toString(), signup_u.getText().toString(), signup_e.getText().toString(), signup_p.getText().toString(), signup_ph.getText().toString(), signup_nc.getText().toString());
+                ServerData sendUser = new ServerData(Splash.this, user,SIGN_UP);
+                sendUser.upload();
+            }
+        });
+        findViewById(R.id.sigin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User.signIn user = new User.signIn( signin_u.getText().toString(), signin_p.getText().toString());
+                ServerData sendUser = new ServerData(Splash.this, user,SIGN_IN);
+                sendUser.upload();
+            }
+        });
+
     }
 
     @Override
     public void onBackPressed() {
         if (signup_Layot.getVisibility() == View.VISIBLE) {
-            signup_Layot.setVisibility(View.GONE);
-            signin.setVisibility(View.VISIBLE);
+            signin_Layot.setVisibility(View.GONE);
+            signin_laybtn.setVisibility(View.VISIBLE);
 
         } else {
             super.onBackPressed();
